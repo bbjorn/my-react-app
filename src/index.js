453a4d24b474b0a884ba8a3c6fb3229f8acf4064
bbjorn <bbjorn@student.chalmers.se>
2020-06-05 17:15:24 +0200

    Tutorial Spread

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
-const address = {
-  street: "",
-  city: "",
-  country: "",
-};
-
 /*
-const street = address.street;
-const city = address.city;
-const country = address.country;
+const first = [1, 2, 3];
+const second = [4, 5, 6];
+
+//const combined = first.concat(second);
+//const combined = [...first, "a", ...second, "b"]; //spread
+
+const clone = [...first];
+console.log(first);
+console.log(clone);
 */
 
-const { street: st, city, country } = adress;
+const first = { name: "Björn" };
+const second = { job: "Designer" };
+
+const combined = { ...first, ...second, location: "Sweden" };
+console.log(combined);
+
+const clone = { ...first };
