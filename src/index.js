cdac1516b66503ba0004c8aa71c2bab5e3f85903
bbjorn <bbjorn@student.chalmers.se>
2020-06-05 17:38:50 +0200

    Named and Default Exports Tutorial
    I completed the js part of the react course!

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
-import { Teacher } from "./teacher";
+import Teacher, { promote } from "./teacher";
+import Reach, { Component } from "react";
+
+// Default -> import ... from '';
+// Named -> import { ... } from '';
 
 const teacher = new Teacher("Mosh", "MSc");
 teacher.teach();
