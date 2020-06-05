9ddea437bd13db33e17cd5e4cad050105780c999
bbjorn <bbjorn@student.chalmers.se>
2020-06-05 16:39:39 +0200

    arrow functions tutorial

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
-// const -> block
-// let -> block
-
-const person = {
-  name: "Björn",
-  walk() {
-    console.log(this);
-  },
+/* 
+const square = function (number) {
+  return number * number;
 };
+*/
 
-const walk = person.walk.bind(person);
-walk();
+const square = (number) => number * number;
+console.log(square(5));
+
+const jobs = [
+  { id: 1, isActive: true },
+  { id: 2, isActive: true },
+  { id: 3, isActive: false },
+];
+
+const activeJobs = jobs.filter(function (job) {
+  return job.isActive;
+});
+const activeJobs = jobs.filter((job) => job.isActive);
