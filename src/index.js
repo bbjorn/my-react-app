ab8f7fb4cfb4eddf0c2db4767913fd7d3fed12bd
bbjorn <bbjorn@student.chalmers.se>
2020-06-05 17:31:34 +0200

    Tutorial on Modules

--- src/index.js
+++ src/index.js
@@ -1,21 +1,4 @@
-class Person {
-  constructor(name) {
-    this.name = name;
-  }
-
-  walk() {
-    console.log("walk");
-  }
-}
-
-class Teacher extends Person {
-  constructor(name, degree) {
-    super(name);
-    this.degree = degree;
-  }
-  teach() {
-    console.log("teach");
-  }
-}
+import { Teacher } from "./teacher";
 
 const teacher = new Teacher("Mosh", "MSc");
+teacher.teach();
