e7cb0ed2d79b2953a06a0ac6ccb3a88e14a55cc6
bbjorn <bbjorn@student.chalmers.se>
2020-06-06 14:01:59 +0200

    Update index.js
    Imported bootstrap.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
-const element = <h1>Hello World!</h1>;
-ReactDOM.render(element, document.getElementById("root"));
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import "bootstrap/dist/css/bootstrap.css";
